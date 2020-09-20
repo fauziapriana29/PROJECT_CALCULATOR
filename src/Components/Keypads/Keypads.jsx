fed54ca14156b2901d17eefd72cd1d963cf096f5
fauziapriana29 <fauzicoding@gmail.com>
2020-09-20 20:07:08 +0700

    try inofation

--- src/Components/Keypads/Keypads.jsx
+++ src/Components/Keypads/Keypads.jsx
@@ -6,21 +6,21 @@ import Input from '../Input/input.jsx'
 const Keypads = () => {
 
   const [display, setDisplay] = useState({
-    data: '',
+    data: '0',
     lastNumber: '',
     equalsNumber: false
   })
   const insert = (num) => {
   
     if (num > 0 && num <= 9) {
-      if (display.equalsNumber === false) {
+      if (display.equalsNumber === false && display.data != 0) {
         console.log(display.equalsNumber)
         setDisplay({
           ...display,
           data: display.data += num,
           lastNumber: display.lastNumber += num
         })
-      } else if (display.equalsNumber === true) {
+      } else if (display.data == 0 || display.equalsNumber === true) {
         console.log(display.equalsNumber)
         // let clearNum = display.data = ''
         setDisplay({
@@ -61,7 +61,7 @@ const Keypads = () => {
         setDisplay({data: strEquals, lastNumber: display.data, equalsNumber: true })
       }
     } else if (num == 'clear') {
-      setDisplay({data: '', lastNumber: '', equalsNumber: false})
+      setDisplay({data: '0', lastNumber: '', equalsNumber: false})
     } else if (num == 'erase') {
       setDisplay({
         data: display.data.substr(0, display.data.length - 1),
